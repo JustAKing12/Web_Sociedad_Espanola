8c4b2c1add1ec8da522cd39eef47ca6bbefd5633
ValentinoBadano <valentinobadano@gmail.com>
2023-10-13 13:15:49 -0300

    first commit

---
+++ src/main/java/ar/edu/unnoba/comunidad/proyecto/ProyectoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.comunidad.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/comunidad/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.comunidad.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
