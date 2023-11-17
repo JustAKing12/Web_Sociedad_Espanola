f471d5315459eb42f9199fc3f0dce0fd418264bb
CHANO <109362877+CHANOCHOCA@users.noreply.github.com>
2023-11-17 15:34:56 -0300

    Borro todo, para limpiar codigo

--- src/main/java/ar/edu/unnoba/Proyecto/repository/NovedadRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unnoba.Proyecto.repository;
-
-import ar.edu.unnoba.Proyecto.model.Novedad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NovedadRepository extends JpaRepository<Novedad, Long> {
-}
--- src/main/java/ar/edu/unnoba/comunidad/proyecto/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unnoba.comunidad.proyecto.repository;
-
-import ar.edu.unnoba.comunidad.proyecto.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-    Evento findEventoByIdevento(Long id);
-    void deleteEventoByIdevento(Long id);
-}
