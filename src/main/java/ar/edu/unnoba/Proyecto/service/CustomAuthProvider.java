aece6ed6081c2e512ff5d0c27940259526df5a03
JmatiF <jmfernandez5@gmail.com>
2023-11-23 17:16:48 -0300

    nueva clase

--- src/main/java/ar/edu/unnoba/Proyecto/service/CustomAuthProvider.java
+++ src/main/java/ar/edu/unnoba/Proyecto/service/CustomAuthProvider.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomAuthProvider implements AuthenticationProvider {
-
+    // Clase para manejar las autenticaciones de manera especial
     @Autowired
     private UserDetailsService userService;
 
