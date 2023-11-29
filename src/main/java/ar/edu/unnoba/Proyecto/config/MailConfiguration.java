ae418bbe617f260e44496177e3007f038c74936d
CHANO <109362877+CHANOCHOCA@users.noreply.github.com>
2023-11-28 22:43:44 -0300

    Add files via upload

---
+++ src/main/java/ar/edu/unnoba/Proyecto/config/MailConfiguration.java
@@ -0,0 +1,34 @@
+package ar.edu.unnoba.Proyecto.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class MailConfiguration {
+    @Value("${spring.mail.username}")
+    private String correo;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(correo);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
