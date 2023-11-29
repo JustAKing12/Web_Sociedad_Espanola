d01c14b977991cec7a4dfbb4b645f99e0e992909
CHANO <109362877+CHANOCHOCA@users.noreply.github.com>
2023-11-28 22:44:59 -0300

    Update MailConfiguration.java

--- src/main/java/ar/edu/unnoba/Proyecto/config/MailConfiguration.java
+++ src/main/java/ar/edu/unnoba/Proyecto/config/MailConfiguration.java
@@ -2,11 +2,13 @@ package ar.edu.unnoba.Proyecto.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@Configuration
 public class MailConfiguration {
     @Value("${spring.mail.username}")
     private String correo;
