5c097a9fc2a55c34a9a262d06c77ff2002717da2
Juan Ignacio Caprioli <109362877+CHANOCHOCA@users.noreply.github.com>
2024-02-27 16:15:24 -0300

    Update UsuarioRepository.java

--- src/main/java/ar/edu/unnoba/Proyecto/repository/UsuarioRepository.java
+++ src/main/java/ar/edu/unnoba/Proyecto/repository/UsuarioRepository.java
@@ -2,10 +2,7 @@ package ar.edu.unnoba.Proyecto.repository;
 
 import ar.edu.unnoba.Proyecto.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
 }
