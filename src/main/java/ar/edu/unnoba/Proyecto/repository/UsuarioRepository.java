d7d13d4003b63d5e97801ecc0a86f9614f7ea28c
Juan Ignacio Caprioli <109362877+CHANOCHOCA@users.noreply.github.com>
2024-03-04 17:56:08 -0300

    Delete src directory

--- src/main/java/ar/edu/unnoba/Proyecto/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unnoba.Proyecto.repository;
-
-import ar.edu.unnoba.Proyecto.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByUsername(String username);
-}
