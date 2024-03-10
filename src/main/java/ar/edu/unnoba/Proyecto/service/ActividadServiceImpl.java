9c6b021a688282f951a6ca5d7a29c58d7fa48a0a
Juan Ignacio Caprioli <109362877+CHANOCHOCA@users.noreply.github.com>
2024-03-10 14:43:51 -0300

    Add files via upload

---
+++ src/main/java/ar/edu/unnoba/Proyecto/service/ActividadServiceImpl.java
@@ -0,0 +1,46 @@
+package ar.edu.unnoba.Proyecto.service;
+
+import ar.edu.unnoba.Proyecto.model.Actividad;
+import ar.edu.unnoba.Proyecto.repository.ActividadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ActividadServiceImpl implements ActividadService {
+
+    private final ActividadRepository actividadRepository;
+
+    @Autowired
+    public ActividadServiceImpl(ActividadRepository actividadRepository) {
+        this.actividadRepository = actividadRepository;
+    }
+
+    @Override
+    public Actividad get(Long id) {
+        return actividadRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Actividad> getAll() {
+        return actividadRepository.findAll();
+    }
+
+    @Override
+    public void save(Actividad actividad) {
+        actividadRepository.save(actividad);
+    }
+
+    @Override
+    public void delete(Long id){
+        actividadRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Actividad> getPage(Pageable pageable) {
+        return actividadRepository.findAll(pageable);
+    }
+}
---
+++ src/main/java/ar/edu/unnoba/Proyecto/service/EventoService.java
@@ -0,0 +1,17 @@
+package ar.edu.unnoba.Proyecto.service;
+
+import ar.edu.unnoba.Proyecto.model.Evento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EventoService {
+    Evento get(Long id);
+    List<Evento> getAll();
+    void save(Evento evento);
+    void delete(Long id);
+
+    Page<Evento> getPage(Pageable pageable);
+}
