f801e7e24f50f734ad72e5ab5c91c403808361e9
Juan Ignacio Caprioli <109362877+CHANOCHOCA@users.noreply.github.com>
2024-03-09 23:01:36 -0300

    Delete src directory

--- src/main/resources/static/script.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtiene el modal
-    var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-    // Abre el modal automáticamente
-    myModal.show();
-});
